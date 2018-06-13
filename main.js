69dd98f75b4c0802676e6622b7bc7635c732c513
ngthing <ngthi.nguyen21@gmail.com>
2018-06-13 16:14:11 -0400

    Get most used char in string

--- main.js
+++ main.js
@@ -7,9 +7,30 @@ var numbersFromInput = function () {
     var numbers = splitedValue.filter(function (v) { return !isNaN(v); });
     return numbers;
 };
+var getSumAndProduct = function () {
+    document.getElementById("sum").innerHTML = numbersFromInput().reduce(function (a, b) { return a + b; }, 0).toString();
+    document.getElementById("product").innerHTML = numbersFromInput().reduce(function (a, b) { return a * b; }, 1).toString();
+};
 var getSum = function () {
     document.getElementById("sum").innerHTML = numbersFromInput().reduce(function (a, b) { return a + b; }, 0).toString();
 };
 var getProduct = function () {
     document.getElementById("product").innerHTML = numbersFromInput().reduce(function (a, b) { return a * b; }, 1).toString();
+};
+var getMostUsedCharacter = function () {
+    var inputValue = document.getElementById("string").value;
+    var mostUsedChar = '';
+    var charTable = {};
+    for (var i = 0; i < inputValue.length; i++) {
+        var c = inputValue[i];
+        if (!charTable[c]) {
+            charTable[c] = 0;
+        }
+        charTable[c]++;
+        if (mostUsedChar === '' || charTable[c] > charTable[mostUsedChar]) {
+            mostUsedChar = c;
+        }
+    }
+    var result = mostUsedChar + " with " + charTable[mostUsedChar] + " times";
+    document.getElementById("mostUsedCharacter").innerHTML = result;
 };
--- main.ts
+++ main.ts
@@ -8,11 +8,32 @@ const numbersFromInput = () => {
   let numbers = splitedValue.filter(v => !isNaN(v))
   return numbers;
 }
-
+const getSumAndProduct = () => {
+  document.getElementById("sum").innerHTML = numbersFromInput().reduce((a, b) => a + b, 0).toString();
+  document.getElementById("product").innerHTML = numbersFromInput().reduce((a, b) => a * b, 1).toString();
+}
 const getSum = () => {
   document.getElementById("sum").innerHTML = numbersFromInput().reduce((a, b) => a + b, 0).toString();
 }
 
 const getProduct = () => {
   document.getElementById("product").innerHTML = numbersFromInput().reduce((a, b) => a * b, 1).toString();
+}
+
+const getMostUsedCharacter = () => {
+  let inputValue = (<HTMLInputElement>document.getElementById("string")).value;
+  let mostUsedChar = '';
+  let charTable = {}
+  for (var i = 0; i < inputValue.length; i++) {
+    var c = inputValue[i];
+    if (!charTable[c]) {
+     charTable[c] = 0 ;
+    }
+    charTable[c]++;
+    if(mostUsedChar === '' || charTable[c] > charTable[mostUsedChar]){
+        mostUsedChar = c;
+    }
+  }
+  let result = `${mostUsedChar} with ${charTable[mostUsedChar]} times`;
+  document.getElementById("mostUsedCharacter").innerHTML = result;
 }
